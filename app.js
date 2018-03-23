b90563d1a4ce54c2cd057120792a18e02ffc13da
Tamas Kuzdi <taku2@betssongroup.com>
2018-03-23 20:39:59 +0100

    include swagger-ui package

---
+++ app.js
@@ -0,0 +1,7 @@
+const SwaggerUI = require('swagger-ui')
+
+SwaggerUI({
+    url: "http://petstore.swagger.io/v2/swagger.json",
+    dom_id: '#main',
+});
+
---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    entry: "./app.js",
+    mode: "production",
+    output: {
+      filename: "bundle.js"
+    }
+  }
