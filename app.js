35c6b9fb01e7b13611f67bdd052ce1e802ff7f49
Küzdi Tamás <kuzditomi@gmail.com>
2018-03-25 10:37:20 +0200

    simplify page

--- app.js
+++
@@ -1,4 +0,0 @@
-const ServiceConfig = require('./service-config');
-const MenuBuilder = require('./menu-builder');
-
-MenuBuilder.build(ServiceConfig);
--- service-config.js
+++
@@ -1,6 +0,0 @@
-module.exports = [
-    {
-        name: 'Trading Tools',
-        url: 'http://petstore.swagger.io/v2/swagger.json'
-    }
-]
